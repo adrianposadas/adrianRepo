413ba563a2dd5569e71d809d637fa6071dfebae5
adrianposadas <posadas.adrian22@gmail.com>
2022-02-02 14:37:01 -0500

    done home, about

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/home', function(req, res, next) {
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('index', { title: 'About me'});
 });
 
 /* GET Projects page. */
