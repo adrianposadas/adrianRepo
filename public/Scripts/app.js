205482713344053409a90153565105373ade329e
adrianposadas <posadas.adrian22@gmail.com>
2022-02-15 17:42:54 -0500

    assignment2 start

---
+++ public/Scripts/app.js
@@ -0,0 +1,16 @@
+/*Adrian Posadas
+301220353
+02/04/2022
+app.js
+*/
+//IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+        
+})();
